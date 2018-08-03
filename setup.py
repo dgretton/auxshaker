8a772b103043f483aa1939254e33796570409f05
Sculpting Evolution <sculptingevo@gmail.com>
2018-08-03 15:53:57 -0400

    auxshaker original source

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auxshaker',
+    version='1.0',
+    packages=find_packages(),
+    license='MIT',
+    description='',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    url='https://github.com/dgretton/auxshaker.git',
+    author='Dana Gretton',
+    author_email='dgretton@mit.edu'
+)
