36e42ae342b255daf20214f60024e05848863fb7
Sculpting Evolution <sculptingevo@gmail.com>
2018-08-07 17:53:05 -0400

    add manifest for config and reorganize

--- auxshaker/__init__.py
+++ auxshaker/__init__.py
@@ -1,7 +1,7 @@
 import os, json
 from os.path import dirname, join
 
-PACKAGE_PATH = dirname(dirname(__file__))
+PACKAGE_PATH = dirname(__file__)
 TEMP_PATH = join(PACKAGE_PATH, 'tmp')
 BAT_PATH = join(PACKAGE_PATH, 'bat')
 with open(join(PACKAGE_PATH, 'config.json')) as f:
